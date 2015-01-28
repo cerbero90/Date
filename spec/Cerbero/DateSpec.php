c2905fee2b62208652f453e605211186141729de
Andrea Marco Sartori <22cerbero22@gmail.com>
2015-01-29 00:58:05 +0100

    Create package

---
+++ spec/Cerbero/DateSpec.php
@@ -0,0 +1,656 @@
+<?php
+
+namespace spec\Cerbero;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use DateTime;
+
+class DateSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Cerbero\Date');
+    }
+
+    /**
+     * @testdox	It creates a date from a string.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_creates_a_date_from_a_string()
+    {
+    	$date = static::create('2000-01-01');
+
+    	$date->shouldBeAnInstanceOf('DateTime');
+
+    	$date->format('d/m/Y')->shouldReturn('01/01/2000');
+    }
+
+    /**
+     * @testdox	It creates multiple dates by passing many parameters.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_creates_multiple_dates_by_passing_many_parameters()
+    {
+    	$dates = static::create('2000-01-01', '2000-01-02');
+
+    	$dates[0]->shouldBeAnInstanceOf('DateTime');
+    	$dates[0]->format('d/m/Y')->shouldReturn('01/01/2000');
+
+    	$dates[1]->shouldBeAnInstanceOf('DateTime');
+    	$dates[1]->format('d/m/Y')->shouldReturn('02/01/2000');
+    }
+
+    /**
+     * @testdox	It creates multiple dates by passing an array.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_creates_multiple_dates_by_passing_an_array()
+    {
+    	$dates = static::create(array('2000-01-01', '2000-01-02'));
+
+    	$dates[0]->shouldBeAnInstanceOf('DateTime');
+    	$dates[0]->format('d/m/Y')->shouldReturn('01/01/2000');
+
+    	$dates[1]->shouldBeAnInstanceOf('DateTime');
+    	$dates[1]->format('d/m/Y')->shouldReturn('02/01/2000');
+    }
+
+    /**
+     * @testdox	It throws an exception when trying to create a date from an invalid string.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_throws_an_exception_when_trying_to_create_a_date_from_an_invalid_string()
+    {
+    	$this->shouldThrow('InvalidArgumentException')->duringCreate('2000.01.01');
+    }
+
+    /**
+     * @testdox	It simply returns the given object when trying to create a date from a DateTime.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_simply_returns_the_given_object_when_trying_to_create_a_date_from_a_DateTime()
+    {
+    	$date = new DateTime('2000-01-01');
+
+    	static::create($date)->shouldReturn($date);
+    }
+
+    /**
+     * @testdox	It checks if a given string is a valid date.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_given_string_is_a_valid_date()
+    {
+    	static::isValid('01/13/2000')->shouldReturn(true);
+
+    	static::isValid('13/01/2000')->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It formats a string of date.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_formats_a_string_of_date()
+    {
+    	static::format('2000-10-01', 'd/m/Y')->shouldReturn('01/10/2000');
+    }
+
+    /**
+     * @testdox	It formats an array of dates.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_formats_an_array_of_dates()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-03');
+		$four  = new DateTime('2000-01-04');
+
+		static::format([$one, $two, $three, $four], 'd/m/Y')->shouldReturn(['01/01/2000', '02/01/2000', '03/01/2000', '04/01/2000']);
+    }
+
+    /**
+     * @testdox	It formats an array of dates of given strings and DateTime objects.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_formats_an_array_of_dates_of_given_strings_and_DateTime_objects()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-03';
+		$four  = new DateTime('2000-01-04');
+
+    	static::format([$one, $two, $three, $four], 'd/m/Y')->shouldReturn(['01/01/2000', '02/01/2000', '03/01/2000', '04/01/2000']);
+    }
+
+    /**
+     * @testdox	It calculates the difference in days between two dates.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_calculates_the_difference_in_days_between_two_dates()
+    {
+    	static::gap('2000-01-01', '2000-01-10')->shouldReturn(9);
+
+    	static::gap('2000-01-10', '2000-01-01')->shouldReturn(9);
+    }
+
+    /**
+     * @testdox	It retrieves the UNIX timestamp of a string.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_UNIX_timestamp_of_a_string()
+    {
+    	static::timestamp('2000-01-01')->shouldReturn(946681200);
+    }
+
+    /**
+     * @testdox	It retrieves the UNIX timestamp of a date.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_UNIX_timestamp_of_a_date()
+    {
+    	static::timestamp(new DateTime('2000-01-01'))->shouldReturn(946681200);
+    }
+
+    /**
+     * @testdox	It retrieves the UNIX timestamp of an array of dates.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_UNIX_timestamp_of_an_array_of_dates()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = '2000-01-02';
+		$three = new DateTime('2000-01-03');
+
+    	static::timestamp([$one, $two, $three])->shouldReturn([946681200, 946767600, 946854000]);
+    }
+
+    /**
+     * @testdox	It checks if two dates are equal.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_two_dates_are_equal()
+    {
+    	static::eq('2000-01-01', '2000-01-01')->shouldReturn(true);
+
+    	static::eq('2000-01-02', '2000-01-01')->shouldReturn(false);
+
+    	static::eq('2000-01-01', '2000-01-02')->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It checks if two dates are not equal.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_two_dates_are_not_equal()
+    {
+    	static::neq('2000-01-01', '2000-01-01')->shouldReturn(false);
+
+    	static::neq('2000-01-02', '2000-01-01')->shouldReturn(true);
+
+    	static::neq('2000-01-01', '2000-01-02')->shouldReturn(true);
+    }
+
+    /**
+     * @testdox	It checks if a date is less than another one.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_date_is_less_than_another_one()
+    {
+    	static::lt('2000-01-01', '2000-01-02')->shouldReturn(true);
+
+    	static::lt('2000-01-02', '2000-01-02')->shouldReturn(false);
+
+    	static::lt('2000-01-02', '2000-01-01')->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It checks if a date is less then or equal to another date.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_date_is_less_then_or_equal_to_another_date()
+    {
+    	static::lte('2000-01-01', '2000-01-02')->shouldReturn(true);
+
+    	static::lte('2000-01-01', '2000-01-01')->shouldReturn(true);
+
+    	static::lte('2000-01-02', '2000-01-01')->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It checks if a date is greater than another one.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_date_is_greater_than_another_one()
+    {
+    	static::gt('2000-01-02', '2000-01-01')->shouldReturn(true);
+
+    	static::gt('2000-01-01', '2000-01-01')->shouldReturn(false);
+
+    	static::gt('2000-01-01', '2000-01-02')->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It checks if a date is greater than or equal to another one.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_date_is_greater_than_or_equal_to_another_one()
+    {
+    	static::gte('2000-01-02', '2000-01-01')->shouldReturn(true);
+
+    	static::gte('2000-01-01', '2000-01-01')->shouldReturn(true);
+
+    	static::gte('2000-01-01', '2000-01-02')->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It retrieves the earliest date of given strings.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_earliest_date_of_given_strings()
+    {
+		$one   = '2000-01-01';
+		$two   = '2000-01-02';
+		$three = '2000-01-02';
+		$four  = '2000-01-03';
+
+    	static::earliest($two, $four, $three, $one)->shouldReturn($one);
+    }
+
+    /**
+     * @testdox	It retrieves the earliest date of given DateTime objects.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_earliest_date_of_given_DateTime_objects()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-02');
+		$four  = new DateTime('2000-01-03');
+
+    	static::earliest($three, $one, $two, $four)->shouldReturn($one);
+    }
+
+    /**
+     * @testdox	It retrieves the earliest date of given strings and objects.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_earliest_date_of_given_strings_and_objects()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::earliest($two, $three, $one, $four)->shouldReturn($one);
+    }
+
+    /**
+     * @testdox	It retrieves the earliest date in a given array.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_earliest_date_in_a_given_array()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::earliest(array($three, $two, $four, $one))->shouldReturn($one);
+    }
+
+    /**
+     * @testdox    It retrieves dates before a given date.
+     *
+     * @author    Andrea Marco Sartori
+     * @return    void
+     */
+    public function it_retrieves_dates_before_a_given_date()
+    {
+        $one   = new DateTime('2000-01-01');
+        $two   = '2000-01-02';
+        $three = new DateTime('2000-01-03');
+        $four  = new DateTime('2000-01-04');
+
+        static::before('2000-01-03', [$one, $two, $three, $four])->shouldReturn([$one, $two]);
+    }
+
+    /**
+     * @testdox    It retrieves dates after a given date.
+     *
+     * @author    Andrea Marco Sartori
+     * @return    void
+     */
+    public function it_retrieves_dates_after_a_given_date()
+    {
+        $one   = new DateTime('2000-01-01');
+        $two   = '2000-01-02';
+        $three = new DateTime('2000-01-03');
+        $four  = new DateTime('2000-01-04');
+
+        static::after('2000-01-01', [$one, $two, $three, $four])->shouldReturn([$two, $three, $four]);
+    }
+
+    /**
+     * @testdox	It retrieves the latest date of given strings.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_latest_date_of_given_strings()
+    {
+		$one   = '2000-01-01';
+		$two   = '2000-01-02';
+		$three = '2000-01-02';
+		$four  = '2000-01-03';
+
+    	static::latest($two, $four, $three, $one)->shouldReturn($four);
+    }
+
+    /**
+     * @testdox	It retrieves the latest date of given DateTime objects.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_latest_date_of_given_DateTime_objects()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-02');
+		$four  = new DateTime('2000-01-03');
+
+    	static::latest($three, $one, $two, $four)->shouldReturn($four);
+    }
+
+    /**
+     * @testdox	It retrieves the latest date of given strings and objects.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_latest_date_of_given_strings_and_objects()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::latest($two, $three, $one, $four)->shouldReturn($four);
+    }
+
+    /**
+     * @testdox	It retrieves the latest date in a given array.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_retrieves_the_latest_date_in_a_given_array()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::latest(array($three, $two, $four, $one))->shouldReturn($four);
+    }
+
+    /**
+     * @testdox	It sorts date strings in ascending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_date_strings_in_ascending_order()
+    {
+		$one   = '2000-01-01';
+		$two   = '2000-01-02';
+		$three = '2000-01-02';
+		$four  = '2000-01-03';
+
+    	static::sort($four, $three, $one, $two)->shouldReturn([$one, $two, $three, $four]);
+    }
+
+    /**
+     * @testdox	It sorts DateTime objects in ascending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_DateTime_objects_in_ascending_order()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-02');
+		$four  = new DateTime('2000-01-03');
+
+    	static::sort($three, $one, $two, $four)->shouldReturn([$one, $three, $two, $four]);
+    }
+
+    /**
+     * @testdox	It sorts strings and DateTime objects in ascending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_strings_and_DateTime_objects_in_ascending_order()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::sort($two, $three, $one, $four)->shouldReturn([$one, $three, $two, $four]);
+    }
+
+    /**
+     * @testdox	It sorts an array of dates in ascending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_an_array_of_dates_in_ascending_order()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::sort(array($three, $two, $four, $one))->shouldReturn([$one, $two, $three, $four]);
+    }
+
+    /**
+     * @testdox	It sorts date strings in decending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_date_strings_in_decending_order()
+    {
+		$one   = '2000-01-01';
+		$two   = '2000-01-02';
+		$three = '2000-01-02';
+		$four  = '2000-01-03';
+
+    	static::reverse($four, $three, $one, $two)->shouldReturn([$four, $three, $two, $one]);
+    }
+
+    /**
+     * @testdox	It sorts DateTime objects in decending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_DateTime_objects_in_decending_order()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-02');
+		$four  = new DateTime('2000-01-03');
+
+    	static::reverse($three, $one, $two, $four)->shouldReturn([$four, $two, $three, $one]);
+    }
+
+    /**
+     * @testdox	It sorts strings and DateTime objects in decending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_strings_and_DateTime_objects_in_decending_order()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::reverse($two, $three, $one, $four)->shouldReturn([$four, $three, $two, $one]);
+    }
+
+    /**
+     * @testdox	It sorts an array of dates in decending order.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_sorts_an_array_of_dates_in_decending_order()
+    {
+    	$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = '2000-01-02';
+		$four  = new DateTime('2000-01-03');
+
+    	static::reverse(array($three, $two, $four, $one))->shouldReturn([$four, $two, $three, $one]);
+    }
+
+    /**
+     * @testdox	It returns a range of dates.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_returns_a_range_of_dates()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-03');
+		$four  = new DateTime('2000-01-04');
+
+    	static::range('2000-01-01', '2000-01-04')->shouldBeLike([$one, $two, $three, $four]);
+    }
+
+    /**
+     * @testdox	It returns a range of dates with the given step.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_returns_a_range_of_dates_with_the_given_step()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-03');
+		$three = new DateTime('2000-01-05');
+		$four  = new DateTime('2000-01-07');
+
+    	static::range('2000-01-01', '2000-01-08', 2)->shouldBeLike([$one, $two, $three, $four]);
+    }
+
+    /**
+     * @testdox	It returns a range of dates even if the start date is greater than the end date.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_returns_a_range_of_dates_even_if_the_start_date_is_greater_than_the_end_date()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-03');
+		$four  = new DateTime('2000-01-04');
+
+    	static::range('2000-01-04', '2000-01-01')->shouldBeLike([$one, $two, $three, $four]);
+    }
+
+    /**
+     * @testdox	It checks if a given string date is included in a given array.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_given_string_date_is_included_in_a_given_array()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-03');
+		$four  = new DateTime('2000-01-04');
+
+    	static::inArray('2000-01-03', [$one, $two, $three, $four])->shouldReturn(true);
+
+    	static::inArray('2000-01-05', [$one, $two, $three, $four])->shouldReturn(false);
+    }
+
+    /**
+     * @testdox	It checks if a given DateTime object is included in a given array.
+     *
+     * @author	Andrea Marco Sartori
+     * @return	void
+     */
+    public function it_checks_if_a_given_DateTime_object_is_included_in_a_given_array()
+    {
+		$one   = new DateTime('2000-01-01');
+		$two   = new DateTime('2000-01-02');
+		$three = new DateTime('2000-01-03');
+		$four  = new DateTime('2000-01-04');
+
+    	static::inArray(new DateTime('2000-01-02'), [$one, $two, $three, $four])->shouldReturn(true);
+
+    	static::inArray(new DateTime('2000-01-05'), [$one, $two, $three, $four])->shouldReturn(false);
+    }
+}
---
+++ src/Date.php
@@ -0,0 +1,392 @@
+<?php namespace Cerbero;
+
+use DateTime;
+use DatePeriod;
+use DateInterval;
+
+/**
+ * Framework agnostic tool to work with dates.
+ *
+ * @author	Andrea Marco Sartori
+ */
+class Date
+{
+
+	/**
+	 * Create one or many dates.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	DateTime|array
+	 */
+	public static function create()
+	{
+		$args = static::listArgs(func_get_args());
+
+		$dates = array_map('static::createDate', $args);
+
+		return static::oneOrMany($dates);
+	}
+
+	/**
+	 * Dynamically retrieve an array of arguments.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	array	$args
+	 * @return	array
+	 */
+	private static function listArgs($args)
+	{
+		return is_array($args[0]) ? $args[0] : $args;
+	}
+
+	/**
+	 * Create a date from a valid string or a DateTime instance.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date
+	 * @return	DateTime
+	 */
+	protected static function createDate($date)
+	{
+		if($date instanceof DateTime) return $date;
+
+		if(static::isValid($date))
+		{
+			return new DateTime($date);
+		}
+
+		throw new \InvalidArgumentException("The provided date is not valid: {$date}");
+	}
+
+	/**
+	 * Retrieve one or many dates depending on the size of the given array.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	array	$dates
+	 * @return	mixed
+	 */
+	private static function oneOrMany(array $dates)
+	{
+		return count($dates) == 1 ? $dates[0] : $dates;
+	}
+
+	/**
+	 * Check if a string date is valid.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string	$date
+	 * @return	boolean
+	 */
+	public static function isValid($date)
+	{
+		return strtotime($date) !== false;
+	}
+
+	/**
+	 * Format a given date or array of dates by using the given format.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime|array	$date
+	 * @param	string	$format
+	 * @return	string|array
+	 */
+	public static function format($date, $format)
+	{
+		$dates = static::forceArray($date);
+
+		foreach ($dates as $date)
+		{
+			$results[] = $date->format($format);
+		}
+
+		return static::oneOrMany($results);
+	}
+
+	/**
+	 * Retrieve always an array even if it deals with only one date.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime|array	$date
+	 * @return	array
+	 */
+	private static function forceArray($date)
+	{
+		$date = static::create($date);
+
+		return is_array($date) ? $date : array($date);
+	}
+
+	/**
+	 * Calculate the difference in days between two dates.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	integer
+	 */
+	public static function gap($date1, $date2)
+	{
+		list($date1, $date2) = static::create($date1, $date2);
+
+		return (int) $date1->diff($date2)->format('%a');
+	}
+
+	/**
+	 * Retrieve the UNIX timestamp of a date or array of dates.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date
+	 * @return	integer
+	 */
+	public static function timestamp($date)
+	{
+		if(is_array($date))
+		{
+			return array_map('static::timestamp', $date);
+		}
+
+		return static::create($date)->getTimestamp();
+	}
+
+	/**
+	 * Check if a date is less than another one.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	boolean
+	 */
+	public static function lt($date1, $date2)
+	{
+		list($date1, $date2) = static::create($date1, $date2);
+
+		return $date1 < $date2;
+	}
+
+	/**
+	 * Check if the first date is less than or equal the second date.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	boolean
+	 */
+	public static function lte($date1, $date2)
+	{
+		list($date1, $date2) = static::create($date1, $date2);
+
+		return $date1 <= $date2;
+	}
+
+	/**
+	 * Check if two dates are equal.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	boolean
+	 */
+	public static function eq($date1, $date2)
+	{
+		list($date1, $date2) = static::create($date1, $date2);
+
+		return $date1 == $date2;
+	}
+
+	/**
+	 * Check if two dates are not equal.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	boolean
+	 */
+	public static function neq($date1, $date2)
+	{
+		return ! static::eq($date1, $date2);
+	}
+
+	/**
+	 * Check if a date is greater than or equal another one.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	boolean
+	 */
+	public static function gte($date1, $date2)
+	{
+		list($date1, $date2) = static::create($date1, $date2);
+
+		return $date1 >= $date2;
+	}
+
+	/**
+	 * Check if a date is greater than another one.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date1
+	 * @param	string|DateTime	$date2
+	 * @return	boolean
+	 */
+	public static function gt($date1, $date2)
+	{
+		list($date1, $date2) = static::create($date1, $date2);
+
+		return $date1 > $date2;
+	}
+
+	/**
+	 * Retrieve the earliest date of the given dates.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	string|DateTime
+	 */
+	public static function earliest()
+	{
+		$dates = static::listArgs(func_get_args());
+
+		return array_reduce($dates, function($carry, $date)
+		{
+			return static::lt($carry, $date) ? $carry : $date;
+
+		}, $dates[0]);
+	}
+
+	/**
+	 * Retrieve the latest date of the given dates.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	string|DateTime
+	 */
+	public static function latest()
+	{
+		$dates = static::listArgs(func_get_args());
+
+		return array_reduce($dates, function($carry, $date)
+		{
+			return static::gt($carry, $date) ? $carry : $date;
+
+		}, $dates[0]);
+	}
+
+	/**
+	 * Retrieve an array of dates before a given date.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date
+	 * @param	array	$dates
+	 * @return	array
+	 */
+	public static function before($date, array $dates)
+	{
+		$filtered = array_filter($dates, function($item) use($date)
+		{
+			return static::lt($item, $date);
+		});
+
+		return array_merge($filtered);
+	}
+
+	/**
+	 * Retrieve an array of dates after a given date.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date
+	 * @param	array	$dates
+	 * @return	array
+	 */
+	public static function after($date, array $dates)
+	{
+		$filtered = array_filter($dates, function($item) use($date)
+		{
+			return static::gt($item, $date);
+		});
+
+		return array_merge($filtered);
+	}
+
+	/**
+	 * Sort the given dates in ascending order.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	array
+	 */
+	public static function sort()
+	{
+		$dates = static::listArgs(func_get_args());
+
+		usort($dates, 'static::gt');
+
+		return $dates;
+	}
+
+	/**
+	 * Sort the given dates in descending order.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @return	array
+	 */
+	public static function reverse()
+	{
+		$dates = static::listArgs(func_get_args());
+
+		usort($dates, 'static::lt');
+
+		return $dates;
+	}
+
+	/**
+	 * Retrieve a range of dates, it's possible to set the step between dates.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$start
+	 * @param	string|DateTime	$end
+	 * @param	integer	$step
+	 * @return	array
+	 */
+	public static function range($start, $end, $step = 1)
+	{
+		list($start, $end) = static::sort($start, $end);
+
+		return iterator_to_array(static::createPeriod($start, $end, $step));
+	}
+
+	/**
+	 * Create a period from $start to $end.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$start
+	 * @param	string|DateTime	$end
+	 * @param	integer	$step
+	 * @return	DatePeriod
+	 */
+	protected static function createPeriod($start, $end, $step)
+	{
+		list($start, $end) = static::create($start, $end);
+
+		$end->modify('+1 day'); // otherwise $end wouldn't be included
+
+		$interval = new DateInterval("P{$step}D");
+
+		return new DatePeriod($start, $interval, $end);
+	}
+
+	/**
+	 * Check if a date is included in a given array.
+	 *
+	 * @author	Andrea Marco Sartori
+	 * @param	string|DateTime	$date
+	 * @param	array	$dates
+	 * @return	boolean
+	 */
+	public static function inArray($date, array $dates)
+	{
+		$date = static::create($date);
+
+		$dates = static::create($dates);
+
+		return array_search($date, $dates) !== false;
+	}
+
+}
